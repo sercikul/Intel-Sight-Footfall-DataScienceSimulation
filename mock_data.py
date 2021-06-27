b00b73bf8debc1432cb017250e2ff658de58487e
sercikul <65176319+sercikul@users.noreply.github.com>
2021-06-27 23:34:41 +0200

    Update mock_data.py

--- mock_data.py
+++ mock_data.py
@@ -1,23 +1,32 @@
 import pandas as pd
 import numpy as np
 import datetime
-import random 
+import random
+
+from pandas.core.frame import DataFrame 
 
 # Event data
 # Normal distribution per venue regarding time ?
 
-targets = {
+# Do function where you can specify all deviceIDs.
 
-}
+# targets = {
+#     '1':  
+#     '2':
+#     '3':
+#     '4':
+# }
 
 attributes = ['targetID', 'deviceID', 'timestamp', 'queueing', 'freeSeats', 'event']
 
-date_rng = pd.date_range(start='1/1/2018', end='now', freq='H')
+date_rng = pd.date_range(start='1/1/2018', end='now', freq='0.2H')
 
-#df = pd.DataFrame(columns = attributes)
+df = pd.DataFrame(columns = attributes)
 
-df = pd.DataFrame(date_rng, columns=['date'])
+# for i in range(len(date_rng)):
+#     df.loc[i] = [i, i, date_rng[i], 10, 4, 'personIn']
 
-df['data'] = np.random.randint(0, 100, size=(len(date_rng)))
+#df = pd.DataFrame(date_rng, columns=['date'])
+#df['data'] = np.random.randint(0, 100, size=(len(date_rng)))
 
 print(df)
